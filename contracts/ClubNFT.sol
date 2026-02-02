36c4ec3befac1a32703cde231b0303e2a77295ca
Duong Phu Dong <dongduong840@gmail.com>
2026-02-02 16:23:42 +0700

    Add whitelist functionality to ClubNFT contract
    Introduces a whitelist mechanism to restrict minting to approved addresses when enabled. Adds functions for managing the whitelist, batch updates, and toggling the whitelist requirement. Updates mint and batchMint functions to enforce whitelist checks via a new modifier.

--- contracts/ClubNFT.sol
+++ contracts/ClubNFT.sol
@@ -21,10 +21,23 @@ contract ClubNFT is ERC721, ERC721URIStorage, Ownable {
     uint256 public mintingFee = 0;
     address public feeRecipient;
 
+    // Whitelist
+    mapping(address => bool) public whitelist;
+    bool public whitelistEnabled = true;
+
     // Events
     event NFTMinted(address indexed to, uint256 indexed tokenId, string uri);
     event MintingFeeUpdated(uint256 newFee);
     event BaseURIUpdated(string newBaseURI);
+    event WhitelistUpdated(address indexed user, bool status);
+    event WhitelistEnabledUpdated(bool enabled);
+
+    modifier onlyWhitelisted() {
+        if (whitelistEnabled) {
+            require(whitelist[msg.sender] || msg.sender == owner(), "Not whitelisted");
+        }
+        _;
+    }
 
     constructor(string memory _baseURI, address _feeRecipient) ERC721("ClubNFT", "CNFT") {
         baseURI = _baseURI;
@@ -36,7 +49,7 @@ contract ClubNFT is ERC721, ERC721URIStorage, Ownable {
      * @param to Address to mint to
      * @param uri IPFS URI for the NFT metadata
      */
-    function mint(address to, string memory uri) external payable returns (uint256) {
+    function mint(address to, string memory uri) external payable onlyWhitelisted returns (uint256) {
         require(msg.value >= mintingFee, "Insufficient payment");
         require(_tokenIdCounter < maxSupply, "Max supply reached");
 
@@ -59,7 +72,7 @@ contract ClubNFT is ERC721, ERC721URIStorage, Ownable {
     /**
      * @dev Batch mint multiple NFTs
      */
-    function batchMint(address to, string[] calldata uris) external payable returns (uint256[] memory) {
+    function batchMint(address to, string[] calldata uris) external payable onlyWhitelisted returns (uint256[] memory) {
         require(msg.value >= mintingFee * uris.length, "Insufficient payment");
         require(_tokenIdCounter + uris.length <= maxSupply, "Max supply exceeded");
 
@@ -91,6 +104,32 @@ contract ClubNFT is ERC721, ERC721URIStorage, Ownable {
     function setMintingFee(uint256 _newFee) external onlyOwner {
         mintingFee = _newFee;
         emit MintingFeeUpdated(_newFee);
+    }
+
+    /**
+     * @dev Add/Remove user from whitelist
+     */
+    function setWhitelist(address user, bool status) external onlyOwner {
+        whitelist[user] = status;
+        emit WhitelistUpdated(user, status);
+    }
+
+    /**
+     * @dev Batch Add/Remove users from whitelist
+     */
+    function batchSetWhitelist(address[] calldata users, bool status) external onlyOwner {
+        for (uint256 i = 0; i < users.length; i++) {
+            whitelist[users[i]] = status;
+            emit WhitelistUpdated(users[i], status);
+        }
+    }
+
+    /**
+     * @dev Toggle whitelist requirement
+     */
+    function setWhitelistEnabled(bool _enabled) external onlyOwner {
+        whitelistEnabled = _enabled;
+        emit WhitelistEnabledUpdated(_enabled);
     }
 
     /**
